f9a9fa1ed56d78db1c84728162e63c45a08fcf7c
Ivan <ivan.yurchenko@kandasoft.com>
2014-07-30 20:49:31 +0300

    git support

--- recipes/upload_module.rb
+++ recipes/upload_module.rb
@@ -21,26 +21,44 @@
 
 upload_module_src_filename = ::File.basename(node['nginx']['upload']['url'])
 upload_module_src_filepath = "#{Chef::Config['file_cache_path']}/#{upload_module_src_filename}"
-upload_module_extract_path = "#{Chef::Config['file_cache_path']}/nginx_upload/#{node['nginx']['upload']['checksum']}"
+upload_module_extract_path = "#{Chef::Config['file_cache_path']}/nginx_upload"
 
-remote_file upload_module_src_filepath do
-  source node['nginx']['upload']['url']
-  checksum node['nginx']['upload']['checksum']
-  owner "root"
-  group "root"
-  mode 00644
+case node['nginx']['upload']['strategy']
+  when 'remote_file'
+    upload_module_extract_path += "/#{node['nginx']['upload']['checksum']}"
+
+    remote_file upload_module_src_filepath do
+      source node['nginx']['upload']['url']
+      checksum node['nginx']['upload']['checksum']
+      owner "root"
+      group "root"
+      mode 00644
+    end
+
+    bash "extract_upload_progress_module" do
+      cwd ::File.dirname(upload_module_src_filepath)
+      code <<-EOH
+        mkdir -p #{upload_module_extract_path}
+        tar xzf #{upload_module_src_filename} -C #{upload_module_extract_path}
+        mv #{upload_module_extract_path}/*/* #{upload_module_extract_path}/
+      EOH
+
+      not_if { ::File.exists?(upload_module_extract_path) }
+    end
+  when 'git'
+    upload_module_extract_path += "/branch-#{node['nginx']['upload']['branch']}"
+
+    git upload_module_extract_path do
+      repository node['nginx']['upload']['url']
+      checkout_branch node['nginx']['upload']['branch']
+      action :checkout
+    end
+  else
+    raise 'Unknown strategy'
 end
 
-bash "extract_upload_progress_module" do
-  cwd ::File.dirname(upload_module_src_filepath)
-  code <<-EOH
-    mkdir -p #{upload_module_extract_path}
-    tar xzf #{upload_module_src_filename} -C #{upload_module_extract_path}
-    mv #{upload_module_extract_path}/*/* #{upload_module_extract_path}/
-  EOH
 
-  not_if { ::File.exists?(upload_module_extract_path) }
-end
+
 
 node.run_state['nginx_configure_flags'] =
   node.run_state['nginx_configure_flags'] | ["--add-module=#{upload_module_extract_path}"]
